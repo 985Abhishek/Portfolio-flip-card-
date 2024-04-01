9ad354fe054aa399d20401fd165ebfa9f0b77fe6
Abhishek Dhange <abhishekdhange@gmail.com>
2024-04-02 00:07:06 +0530

    uploaded

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+// let project = document.querySelector(".flip-card");
+// let audio = document.querySelector("#myAudio");
+
+// project.addEventListener("mouseover", function() {
+//   audio.play();
+// });
